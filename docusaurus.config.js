0c574dcdd6bee5c23c6d44e041428add3dd8e0d1
nerijus-isun <106319614+nerijus-isun@users.noreply.github.com>
2023-10-19 09:56:28 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   organizationName,
   projectName,
   // Set the production url of your site here
-  url: "https://docs.globalpass.ch",
+  url: "https://developer.globalpass.ch",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
